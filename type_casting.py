204c492ae81f9b1e1738d0168ee53ade0ac8ba34
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-08 22:43:03 +0545

    python type casting implicit & explicit

---
+++ type_casting.py
@@ -0,0 +1,25 @@
+# IMPLICIT
+a = 10
+b = 20.22
+c = a+b
+print(c)
+
+# EXPLICIT
+
+a = 10
+print(float(a))
+
+b = 20.56
+print(int(b))
+
+c = "111000111"
+print(int(c, 2))
+print(int(c, 8))
+
+d = "2A9"
+print(int(d, 16))
+
+bool = True
+# print(bool)
+# print(str(bool))
+print(type(str(bool)))
