e0103e09dd0e4c5d7baac8971266ba86dee5a2f0
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-08 23:52:27 +0545

    booleans

---
+++ booleans.py
@@ -0,0 +1,17 @@
+a = True
+b = False
+print(bool(a))
+print(bool(b))
+
+n = None
+print(bool(n))
+
+str = 'aasdflkj'
+print(bool(str))
+str2 = ''
+print(bool(str2))
+
+comp = complex(False)
+print(comp)
+comp2 = complex(True)
+print(comp2)
