aa2dc7add496d45a5db81b6ec4a9ff68500f6374
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-08 23:56:33 +0545

    control statements: if,elif & else

---
+++ control_stmts.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age:"))
+if(age < 18):
+    print('Oops you are underaged !')
+elif(age == 18):
+    print('Congrats on turning 18 !')
+else:
+    print('So sad you are so old now !')
