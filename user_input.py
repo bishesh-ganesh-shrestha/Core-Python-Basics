c3f660cf475d8624125c3d1e4bfaa7e6f903e1c6
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-08 23:47:03 +0545

    user input operations

---
+++ user_input.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number:"))
+operator = input("Enter the type of operation to perform: + or - ??")
+
+if operator == '+':
+    print(num1+num2)
+elif operator == '-':
+    print(num1-num2)
+else:
+    print("Invalid operation")
