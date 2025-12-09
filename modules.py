105f2a9334bc60a8d3c4700dcd0fd470d8521697
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-09 17:03:30 +0545

    modules in py

---
+++ modules.py
@@ -0,0 +1,16 @@
+# import func_annotations
+
+# func_annotations.math(5, 15)
+
+# from my_module import *
+# print(sum(1,2))
+# print(subtract(5,3))
+# print(multiply(2,3))
+# print(divide(8,4))
+
+import my_module
+print(my_module.sum(1,2))
+
+print(my_module.__name__)
+print(my_module.__file__)
+print(my_module.__doc__)
