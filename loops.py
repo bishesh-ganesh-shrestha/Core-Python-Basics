28d79e355b8c6fb96d6ccc93d1bd7289e6cbad15
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-09 15:52:59 +0545

    loops:for, for-else, ...

---
+++ loops.py
@@ -0,0 +1,22 @@
+# FOR LOOP
+
+# dict = {
+#     'name': 'Jack',
+#     'age': 26,
+#     'city': 'New York',
+#     'profession': 'Developer'
+# }
+
+# for key,value in dict.items():
+#     print(key, end= ': ')
+#     print(value)
+    
+# FOR-ELSE
+
+arr = ('name', 'age', 'city', 'profession')
+for a in arr:
+    print(a)
+    break
+else:
+    print('Loop completed without break')
+print('Out of loop now')
