d41681fbd7d878177385fe284c9902653ab38726
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-09 13:11:48 +0545

    control statements: if else, match case, loops

--- control_stmts.py
+++ control_stmts.py
@@ -1,7 +1,34 @@
-age = int(input("Enter your age:"))
-if(age < 18):
-    print('Oops you are underaged !')
-elif(age == 18):
-    print('Congrats on turning 18 !')
-else:
-    print('So sad you are so old now !')
+# age = int(input("Enter your age:"))
+# if(age < 18):
+#     print('Oops you are underaged !')
+# elif(age == 18):
+#     print('Congrats on turning 18 !')
+# else:
+#     print('So sad you are so old now !')
+    
+# alphabet = (input("Enter an alphabet letter to see if it is vowel or not:"))
+
+# match alphabet:
+#     case 'a' | 'e' | 'i' | 'o' | 'u':
+#         print ('Yes it is vowel')
+#     case _:
+#         print('No it is not a vowel')
+        
+# list = [1,2,'abc','xyz','3',4,5]
+# for x in list:
+#     print(x, end=" ")
+    
+# x = 1
+# while (x <= 10):
+#     print(x, end=" ")
+#     x += 1
+
+# x=0
+# while x < 5:
+#     print(x)
+#     if x == 3:
+#         break
+#     x+=1
+    
+
+    
